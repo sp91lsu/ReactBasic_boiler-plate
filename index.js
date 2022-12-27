cbceb8e123be75cafa516aeb0b5dc9603f351d72
dev웅 <dev.lsw91@gmail.com>
2022-12-27 14:38:56 +0900

    #3 몽고 DB 연결
    몽고DB 사이트에 클러스터 만들기.
    username, password 입력.
    현재 IP적용.
    Connect - your application.
    connection string 복사.
    
    Mongoose 다운로드 - npm install mongoose
    mongoose 연결 코드 작성.
    
    packge.json에 "start": "node index.js" 스크립트 작성.
    npm run start

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 6000
+
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://swlee:1234@boilerplate.mklpftc.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser:true, useUnifiedTopology:true
+}).then(()=> console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
 
 app.get('/', (req, res) => res.send('Hello World!~~안녕하세요'))
 
